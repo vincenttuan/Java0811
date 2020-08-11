de8b882c9ddfd42f40f1ea0a2291b565e5f0e03d
MB-teacher <MB-teacher@192.168.1.157>
2020-08-11 14:14:09 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d01/ArrayTest2.java
@@ -0,0 +1,11 @@
+package com.pcschool.ocp.d01;
+
+public class ArrayTest2 {
+    public static void main(String[] args) {
+        // 股票獲利
+        double[] stocks = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};
+        // 債券獲利
+        double[] bounds = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.9, 7.1, 7.2};
+        
+    }
+}
