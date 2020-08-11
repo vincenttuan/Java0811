7e54f4a86a400dcaeb8b7ab525c413f012b6f574
MB-teacher <MB-teacher@192.168.1.157>
2020-08-11 11:52:40 +0800

    Java

--- src/main/java/com/pcschool/ocp/d01/ATM.java
+++ src/main/java/com/pcschool/ocp/d01/ATM.java
@@ -1,13 +1,42 @@
 package com.pcschool.ocp.d01;
 
+import java.util.Scanner;
+
 public class ATM {
     
     public static void main(String[] args) {
-        run();
+        Account account = new Account();
+        run(account);
     }
     
-    public static void run() {
-        System.out.println("run....");
+    public static void run(Account account) {
+        System.out.println("1.存款");
+        System.out.println("2.提款");
+        System.out.println("3.查詢");
+        System.out.println("9.離開");
+        System.out.println("---------");
+        System.out.print("請選擇: ");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch(choice) {
+            case 1: // 1.存款
+                System.out.print("請輸入存款金額: ");
+                int saveMoney = sc.nextInt();
+                account.save(saveMoney);
+                break;
+            case 2: // 2.提款
+                System.out.print("請輸入提款金額: ");
+                int withdrawMoney = sc.nextInt();
+                account.withdraw(withdrawMoney);
+                break;
+            case 3: // 3.查詢
+                int balance = account.getBalance();
+                System.out.printf("帳戶餘額: %,d\n", balance);
+                break;
+            case 9: // 9.離開
+                return;
+        }
+        run(account); // 遞迴
     }
     
 }
