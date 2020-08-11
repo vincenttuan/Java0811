e71e2b71201703f5259659c815079de7e0abf04d
MB-teacher <MB-teacher@192.168.1.157>
2020-08-11 11:20:17 +0800

    Java

--- src/main/java/com/pcschool/ocp/d01/BMI.java
+++ src/main/java/com/pcschool/ocp/d01/BMI.java
@@ -4,11 +4,15 @@ public class BMI {
     public String name;
     public double h;
     public double w;
-    public double bmi;
+    private double bmi; // 私有物件變數
     
-    // 計算 BMI 的方法
-    public double calcBMI() {
-        bmi = w / Math.pow(h/100, 2);
+    // 取得 bmi 的值
+    public double getBMI() {
         return bmi;
+    }
+    
+    // 設定/計算 BMI 值的方法
+    public void setBMI() {
+        bmi = w / Math.pow(h/100, 2);
     }
 }
--- src/main/java/com/pcschool/ocp/d01/BMIStore.java
+++ src/main/java/com/pcschool/ocp/d01/BMIStore.java
@@ -4,13 +4,13 @@ public class BMIStore {
     public static void main(String[] args) {
         BMI b1 = new BMI();
         b1.name="John";b1.h=170;b1.w=60;
-        b1.bmi = b1.calcBMI();
+        b1.setBMI();
         
         BMI b2 = new BMI();
         b2.name="Mary";b2.h=165;b2.w=45;
-        b2.bmi = -1;
+        b2.setBMI();
         
-        System.out.printf("%s (%.1f, %.1f) bmi=%.2f\n", b1.name, b1.h, b1.w, b1.bmi);
-        System.out.printf("%s (%.1f, %.1f) bmi=%.2f\n", b2.name, b2.h, b2.w, b2.bmi);
+        System.out.printf("%s (%.1f, %.1f) bmi=%.2f\n", b1.name, b1.h, b1.w, b1.getBMI());
+        System.out.printf("%s (%.1f, %.1f) bmi=%.2f\n", b2.name, b2.h, b2.w, b2.getBMI());
     }
 }
