7d0ea54f6f020ca850af40905b3a7a944533bafd
MB-teacher <MB-teacher@192.168.1.157>
2020-08-11 15:32:06 +0800

    Java

--- src/main/java/com/pcschool/ocp/d01/ArrayTest3.java
+++ src/main/java/com/pcschool/ocp/d01/ArrayTest3.java
@@ -1,5 +1,7 @@
 package com.pcschool.ocp.d01;
 
+import java.util.Arrays;
+
 public class ArrayTest3 {
     public static void main(String[] args) {
         double[] h = {172, 168, 164, 170, 176};
@@ -11,8 +13,16 @@ public class ArrayTest3 {
         }
         avg = sum/h.length;
         System.out.printf("平均: %.2f cm\n", avg);
+        // 取最大值?
+        
         
         // Java 8
+        // 列出所有身高
+        Arrays.stream(h).forEach(x -> System.out.println(x));
+        // 計算總和與平均
+        sum = Arrays.stream(h).sum();
+        avg = Arrays.stream(h).average().getAsDouble();
+        System.out.printf("總身高: %.1f 平均身高: %.1f\n", sum, avg);
         
     }
  
