c1daec2d2d243a3f3540ad669e9157fc7e60c117
MB-teacher <MB-teacher@192.168.1.157>
2020-08-11 10:10:32 +0800

    Java

--- src/main/java/com/pcschool/ocp/d01/Dog.java
+++ src/main/java/com/pcschool/ocp/d01/Dog.java
@@ -1,7 +1,7 @@
 package com.pcschool.ocp.d01;
 
 public class Dog {
-    public String type; // 品種
+    public static String type; // 品種
     public int age;     // 年齡
     public int price;   // 價格
     public int amount;  // 數量
--- src/main/java/com/pcschool/ocp/d01/PetStore.java
+++ src/main/java/com/pcschool/ocp/d01/PetStore.java
@@ -8,16 +8,17 @@ public class PetStore {
         Dog d2 = new Dog();
         Dog d3 = new Dog();
         // d1, d2, d3 資料設定
-        d1.type = "拉不拉多";d1.age = 5;d1.price = 5000;d1.amount = 30;
-        d2.type = "拉不拉多";d2.age = 2;d2.price = 3000;d2.amount = 20;
-        d3.type = "拉不拉多";d3.age = 4;d3.price = 8000;d3.amount = 10;
+        Dog.type = "拉不拉多";
+        d1.age = 5;d1.price = 5000;d1.amount = 30;
+        d2.age = 2;d2.price = 3000;d2.amount = 20;
+        d3.age = 4;d3.price = 8000;d3.amount = 10;
         // d1, d2, d3 資料印出
         System.out.printf("品種: %s 年齡: %d 價格: %,d 數量: %d 小計: %,d\n", 
-                          d1.type, d1.age, d1.price, d1.amount, (d1.price*d1.amount));
+                          Dog.type, d1.age, d1.price, d1.amount, (d1.price*d1.amount));
         System.out.printf("品種: %s 年齡: %d 價格: %,d 數量: %d 小計: %,d\n", 
-                          d2.type, d2.age, d2.price, d2.amount, (d2.price*d2.amount));
+                          Dog.type, d2.age, d2.price, d2.amount, (d2.price*d2.amount));
         System.out.printf("品種: %s 年齡: %d 價格: %,d 數量: %d 小計: %,d\n", 
-                          d3.type, d3.age, d3.price, d3.amount, (d3.price*d3.amount));
+                          Dog.type, d3.age, d3.price, d3.amount, (d3.price*d3.amount));
         // d1, d2, d3 的小計總和
         int sum = (d1.price*d1.amount) + (d2.price*d2.amount) + (d3.price*d3.amount);
         System.out.printf("Dog 總資產: %,d\n", sum);
