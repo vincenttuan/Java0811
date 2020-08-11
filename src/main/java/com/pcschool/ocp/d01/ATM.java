61f833a884f34e319640408614aac3ce10fd4019
MB-teacher <MB-teacher@192.168.1.157>
2020-08-11 11:41:42 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d01/ATM.java
@@ -0,0 +1,13 @@
+package com.pcschool.ocp.d01;
+
+public class ATM {
+    
+    public static void main(String[] args) {
+        run();
+    }
+    
+    public static void run() {
+        System.out.println("run....");
+    }
+    
+}
