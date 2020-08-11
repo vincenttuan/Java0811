fe490db0adb0102ddd02f0f4a1f2123d61213b51
MB-teacher <MB-teacher@192.168.1.157>
2020-08-11 11:09:25 +0800

    Java

--- src/main/java/com/pcschool/ocp/d01/BMI.java
+++ src/main/java/com/pcschool/ocp/d01/BMI.java
@@ -5,4 +5,10 @@ public class BMI {
     public double h;
     public double w;
     public double bmi;
+    
+    // 計算 BMI 的方法
+    public double calcBMI() {
+        bmi = w / Math.pow(h/100, 2);
+        return bmi;
+    }
 }
--- src/main/java/com/pcschool/ocp/d01/BMIStore.java
+++ src/main/java/com/pcschool/ocp/d01/BMIStore.java
@@ -1,5 +1,16 @@
 package com.pcschool.ocp.d01;
 
 public class BMIStore {
-    
+    public static void main(String[] args) {
+        BMI b1 = new BMI();
+        b1.name="John";b1.h=170;b1.w=60;
+        b1.bmi = b1.calcBMI();
+        
+        BMI b2 = new BMI();
+        b2.name="Mary";b2.h=165;b2.w=45;
+        b2.bmi = b2.calcBMI();
+        
+        System.out.printf("%s bmi=%.2f\n", b1.name, b1.bmi);
+        System.out.printf("%s bmi=%.2f\n", b2.name, b2.bmi);
+    }
 }
