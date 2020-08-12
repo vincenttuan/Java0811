d261b802cfbaf0f9ce68720fa83fcbe75ad2b566
MB-teacher <MB-teacher@192.168.1.157>
2020-08-12 11:27:16 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d02/StringTest2.java
@@ -0,0 +1,10 @@
+package com.pcschool.ocp.d02;
+
+public class StringTest2 {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+    }
+}
