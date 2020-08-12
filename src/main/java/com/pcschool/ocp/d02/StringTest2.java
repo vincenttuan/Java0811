112759c1734ac146a8a7fe34a1fc00fe0fce68f8
MB-teacher <MB-teacher@192.168.1.157>
2020-08-12 11:33:33 +0800

    Java

--- src/main/java/com/pcschool/ocp/d02/StringTest2.java
+++ src/main/java/com/pcschool/ocp/d02/StringTest2.java
@@ -4,7 +4,11 @@ public class StringTest2 {
     public static void main(String[] args) {
         String s1 = "Java";
         String s2 = "Java";
+        String s3 = new String("Java"); // s3 在 pool 外面
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
+        System.out.println(s1 == s3);
+        s3 = s3.intern(); // s3 在 pool 裡面
+        System.out.println(s1 == s3);
     }
 }
