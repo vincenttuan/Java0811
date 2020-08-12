fc29df79115d0b74174ecfd3aef6153cc754b4ea
MB-teacher <MB-teacher@192.168.1.157>
2020-08-12 14:10:32 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d02/MultiArray5.java
@@ -0,0 +1,15 @@
+package com.pcschool.ocp.d02;
+
+public class MultiArray5 {
+    public static void main(String[] args) {
+        char[][] ttt = {
+                        {'O', 'O', 'X'},
+                        {'X', 'O', ' '},
+                        {'X', ' ', 'X'}
+                       };
+        ttt[?][?] = 'O';
+        System.out.println("O 勝利");
+        // 印出最後結局
+        
+    }
+}
