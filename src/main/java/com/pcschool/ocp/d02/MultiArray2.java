e9d2648adf1b373ff68c02a97b2480a4776b03eb
MB-teacher <MB-teacher@192.168.1.157>
2020-08-12 13:37:25 +0800

    Java

--- src/main/java/com/pcschool/ocp/d02/MultiArray2.java
+++ src/main/java/com/pcschool/ocp/d02/MultiArray2.java
@@ -3,6 +3,7 @@ package com.pcschool.ocp.d02;
 public class MultiArray2 {
     public static void main(String[] args) {
         int[][] x = {{100, 90, 80}, {70, 60, 50}};
+        System.out.println("x length = " + x.length);
         // 將 x[0] 的內容取出
         for(int i=0;i<x[0].length;i++) {
             System.out.println(x[0][i]);
