2f42bdf5ad0719838363041fe939c0249bb8c0a9
MB-teacher <MB-teacher@192.168.1.157>
2020-08-12 11:46:44 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d02/StringTest3.java
@@ -0,0 +1,10 @@
+package com.pcschool.ocp.d02;
+
+public class StringTest3 {
+    public static void main(String[] args) {
+        // 可變字串
+        StringBuilder s = new StringBuilder("Java");
+        s.append("8");
+        System.out.println(s);
+    }
+}
