fa59284ffb9403fb42bcca9081012819b75a8614
MB-teacher <MB-teacher@192.168.1.157>
2020-08-12 10:08:05 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/game/English.java
@@ -0,0 +1,15 @@
+package com.pcschool.ocp.game;
+
+import java.util.Random;
+
+public class English {
+    private int ans = new Random().nextInt(26) + 65;
+    private int count;
+    public int check(int userAns) {
+        count++;
+        return userAns - ans;
+    }
+    public int getCount() {
+        return count;
+    }
+}
--- src/main/java/com/pcschool/ocp/game/JavaGame.java
+++ src/main/java/com/pcschool/ocp/game/JavaGame.java
@@ -3,8 +3,29 @@ package com.pcschool.ocp.game;
 import java.util.Scanner;
 
 public class JavaGame {
-
     public static void main(String[] args) {
+        englishGameLoop();
+    }
+    
+    public static void englishGameLoop() {
+        English english = new English();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("請輸入英文字: ");
+            int userAns = sc.next().toUpperCase().charAt(0);
+            int result = english.check(userAns);
+            if (result == 0) {
+                System.out.println("恭喜答對了");
+                break;
+            } else {
+                System.out.printf("猜第 %d 次, 結果: 猜 %s 了\n",
+                        english.getCount(), result > 0 ? "大" : "小");
+                //continue;
+            }
+        }
+    }
+    
+    public static void numberGameLoop() {
         Number number = new Number();
         while (true) {
             Scanner sc = new Scanner(System.in);
