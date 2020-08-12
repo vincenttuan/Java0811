ea15569eee641bb74946892250fda622e8e5ab86
MB-teacher <MB-teacher@192.168.1.157>
2020-08-12 10:28:48 +0800

    Java

--- src/main/java/com/pcschool/ocp/game/JavaGame.java
+++ src/main/java/com/pcschool/ocp/game/JavaGame.java
@@ -4,7 +4,32 @@ import java.util.Scanner;
 
 public class JavaGame {
     public static void main(String[] args) {
-        englishGameLoop();
+        menu();
+    }
+    
+    public static void menu() {
+        while (true) {            
+            System.out.println("Java Game");
+            System.out.println("---------------");
+            System.out.println("1. 猜數字");
+            System.out.println("2. 猜字母");
+            System.out.println("9. Exit");
+            System.out.println("---------------");
+            System.out.print("請選擇 ==> ");
+            int choice = new Scanner(System.in).nextInt();
+            switch(choice) {
+                case 1:
+                    numberGameLoop();
+                    break;
+                case 2:
+                    englishGameLoop();
+                    break;    
+                case 9:
+                    return; // menu() 方法結束
+            }
+            System.out.println("請按下 Enter 鍵繼續 ...");
+            new Scanner(System.in).nextLine();
+        }
     }
     
     public static void englishGameLoop() {
