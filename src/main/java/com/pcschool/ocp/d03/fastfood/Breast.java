073ec890b0aaba48dcc79f1b722f1e6d3fb7c10e
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 13:25:01 +0800

    Java

--- src/main/java/com/pcschool/ocp/d03/fastfood/Breast.java
+++ src/main/java/com/pcschool/ocp/d03/fastfood/Breast.java
@@ -2,7 +2,12 @@ package com.pcschool.ocp.d03.fastfood;
 
 public class Breast {
     private int price = 35;
+    private static int amount = 100;
     public int getPrice() {
         return price;
-    }    
+    }
+    public static int getAmount() {
+        return amount;
+    }
+    
 }
--- src/main/java/com/pcschool/ocp/d03/fastfood/Coca.java
+++ src/main/java/com/pcschool/ocp/d03/fastfood/Coca.java
@@ -2,7 +2,11 @@ package com.pcschool.ocp.d03.fastfood;
 
 public class Coca {
     private int price = 20;
+    private static int amount = 100;
     public int getPrice() {
         return price;
+    }
+    public static int getAmount() {
+        return amount;
     }    
 }
