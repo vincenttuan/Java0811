454033f13dcab8341b7c11fc50a6b27079721491
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 09:54:00 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/game/case3/Poker.java
@@ -0,0 +1,14 @@
+package com.pcschool.ocp.game.case3;
+
+public class Poker {
+    public String mark; // 花色
+    public String number; // 號碼
+    public int    value; // 價值
+
+    public Poker(String mark, String number, int value) {
+        this.mark = mark;
+        this.number = number;
+        this.value = value;
+    }
+    
+}
