c8ada8e21c5a231a98373ced9345f00f80f486c4
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 13:23:46 +0800

    Java

--- src/main/java/com/pcschool/ocp/d03/bookstore/Store.java
+++ src/main/java/com/pcschool/ocp/d03/bookstore/Store.java
@@ -3,6 +3,14 @@ package com.pcschool.ocp.d03.bookstore;
 public class Store {
     public static void main(String[] args) {
         Pen[] pens = new Pen[4];
-        
+        for(int i=0;i<pens.length;i++) {
+            pens[i] = new Pen();
+        }
+        int sum = 0;
+        for(Pen pen : pens) {
+            sum += pen.getPrice();
+        }
+        System.out.println(sum);
+        System.out.println("剩下: " + Pen.getAmount());
     }
 }
