c30a51bf712b85cd5721ba76b3a4721774838821
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 09:55:59 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/game/case3/PokerMain.java
@@ -0,0 +1,8 @@
+package com.pcschool.ocp.game.case3;
+
+public class PokerMain {
+    public static void main(String[] args) {
+        // ♠ ♥ ♦ ♣
+        Poker p = new Poker("♠", "A", 1);
+    }
+}
