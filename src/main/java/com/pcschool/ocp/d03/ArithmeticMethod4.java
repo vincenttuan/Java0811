a6ac90aecff35995e87726dc2657721767284633
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 15:49:30 +0800

    Java

--- src/main/java/com/pcschool/ocp/d03/ArithmeticMethod4.java
+++ src/main/java/com/pcschool/ocp/d03/ArithmeticMethod4.java
@@ -2,7 +2,14 @@ package com.pcschool.ocp.d03;
 
 public class ArithmeticMethod4 {
     public static void main(String[] args) {
-        
+        Exam[] exams = {
+            new Exam(100),
+            new Exam(50),
+            new Exam(80),
+            new Exam(90),
+            new Exam(40),
+        };
+        // 利用 Java 8 列出及格分數且由小至大排序 
     }
 }
 
