501ad1f8422bcd1dd91e473c58cf0a8c36ed60d7
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 15:45:35 +0800

    Java

--- src/main/java/com/pcschool/ocp/d03/ArithmeticMethod3.java
+++ src/main/java/com/pcschool/ocp/d03/ArithmeticMethod3.java
@@ -1,6 +1,7 @@
 package com.pcschool.ocp.d03;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ArithmeticMethod3 {
     public static void main(String[] args) {
@@ -11,6 +12,14 @@ public class ArithmeticMethod3 {
         };
         // Java 8 資料列表
         Arrays.stream(bmis).forEach(b -> System.out.println(b));
+        System.out.println("----------------------------------");
+        Arrays.stream(bmis)
+                .filter(b -> b.getValue() >= 18 && b.getValue() <= 23)
+                .forEach(b -> System.out.println(b));
+        System.out.println("----------------------------------");
+        Arrays.stream(bmis)
+                .sorted(Comparator.comparing(BMI::getValue))
+                .forEach(b -> System.out.println(b));
     }
 }
 
@@ -22,6 +31,7 @@ class BMI {
     public BMI(double h, double w) {
         this.h = h;
         this.w = w;
+        value = w / Math.pow(h/100, 2);
     }
 
     public double getH() {
