85daad9e6f3bdbe4bf8116700c985b9320809171
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 10:11:45 +0800

    Java

--- src/main/java/com/pcschool/ocp/game/case3/PokerMain.java
+++ src/main/java/com/pcschool/ocp/game/case3/PokerMain.java
@@ -2,14 +2,15 @@ package com.pcschool.ocp.game.case3;
 
 public class PokerMain {
     public static void main(String[] args) {
-        // ♠ ♥ ♦ ♣
+        // ♠S ♥H ♦D ♣C
         // Black: \u001b[30m 
         // Red: \u001b[31m
-        Poker p1 = new Poker("♠", "A", "\u001b[30m", 1);
-        Poker p2 = new Poker("♥", "A", "\u001b[31m", 1);
+        Poker p1 = new Poker("S", "A", "\u001b[30m", 1);
+        Poker p2 = new Poker("H", "A", "\u001b[31m", 1);
         
         System.out.println(p1);
         System.out.println(p2);
+        System.out.println("♠");
     }
 }
 
