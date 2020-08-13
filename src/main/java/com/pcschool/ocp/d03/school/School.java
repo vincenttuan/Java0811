9e4d0fd7b3b843aa59a5409b3a6dc9279992ca52
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 13:51:39 +0800

    Java

--- src/main/java/com/pcschool/ocp/d03/school/School.java
+++ src/main/java/com/pcschool/ocp/d03/school/School.java
@@ -2,6 +2,11 @@ package com.pcschool.ocp.d03.school;
 
 public class School {
     public static void main(String[] args) {
-        
+        String[][] data = {
+            {"john", "18", "170" , "60"},
+            {"mary", "17", "160" , "45"},
+            {"jane", "17", "165" , "65"},
+            {"mark", "19", "180" , "55"},
+        };
     }
 }
