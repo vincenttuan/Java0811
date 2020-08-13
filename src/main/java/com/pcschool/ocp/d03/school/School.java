76fd3b35421bced0f867171b7ddeb40554f37366
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 14:12:40 +0800

    Java

--- src/main/java/com/pcschool/ocp/d03/school/School.java
+++ src/main/java/com/pcschool/ocp/d03/school/School.java
@@ -8,7 +8,20 @@ public class School {
             {"jane", "17", "165" , "65"},
             {"mark", "19", "180" , "55"},
         };
-        System.out.println(data.length);
+        
+        Student[] students = new Student[data.length];
+        for(int i=0;i<data.length;i++) {
+            String[] row = data[i];
+            String name   = row[0];
+            int    age    = Integer.parseInt(row[1]);
+            double height = Double.parseDouble(row[2]);
+            double weight = Double.parseDouble(row[3]);
+            // 建立 BMI 物件
+            BMI bmi = new BMI(height, weight);
+            // 建立 Student 物件
+            Student student = new Student(name, age, bmi);
+            System.out.println(student);
+        }
         
         
     }
