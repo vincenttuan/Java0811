5ea9079b770739a1702f1da826f6cd1c5c551f9c
MB-teacher <MB-teacher@192.168.1.157>
2020-08-13 09:58:20 +0800

    Java

--- src/main/java/com/pcschool/ocp/game/case3/Poker.java
+++ src/main/java/com/pcschool/ocp/game/case3/Poker.java
@@ -3,12 +3,14 @@ package com.pcschool.ocp.game.case3;
 public class Poker {
     public String mark; // 花色
     public String number; // 號碼
+    public String color; // 顏色
     public int    value; // 價值
 
-    public Poker(String mark, String number, int value) {
+    public Poker(String mark, String number, String color, int value) {
         this.mark = mark;
         this.number = number;
+        this.color = color;
         this.value = value;
     }
-    
+
 }
--- src/main/java/com/pcschool/ocp/game/case3/PokerMain.java
+++ src/main/java/com/pcschool/ocp/game/case3/PokerMain.java
@@ -3,6 +3,8 @@ package com.pcschool.ocp.game.case3;
 public class PokerMain {
     public static void main(String[] args) {
         // ♠ ♥ ♦ ♣
-        Poker p = new Poker("♠", "A", 1);
+        // Black: \u001b[30m 
+        // Red: \u001b[31m
+        Poker p = new Poker("♠", "A", "\u001b[30m", 1);
     }
 }
