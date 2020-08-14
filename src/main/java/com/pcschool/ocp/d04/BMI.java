afa39585e3eba3ce710632427e0e1f85cb33f9dc
MB-teacher <MB-teacher@192.168.1.157>
2020-08-14 10:15:36 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d04/BMI.java
@@ -0,0 +1,12 @@
+package com.pcschool.ocp.d04;
+
+public class BMI {
+    private double h; // 100 ~ 250 cm (170.0)
+    private double w; // 30 ~ 150 kg  (60.0)
+
+    @Override
+    public String toString() {
+        return "BMI{" + "h=" + h + ", w=" + w + '}';
+    }
+
+}
---
+++ src/main/java/com/pcschool/ocp/d04/ConstructorDemo4.java
@@ -0,0 +1,15 @@
+package com.pcschool.ocp.d04;
+
+public class ConstructorDemo4 {
+    public static void main(String[] args) {
+        BMI b1 = new BMI();
+        BMI b2 = new BMI(180, 180);
+        BMI b3 = new BMI(60, 170);
+        BMI b4 = new BMI(180, 70);
+        
+        System.out.println(b1); // 170, 60
+        System.out.println(b2); // 180, 60
+        System.out.println(b3); // 170, 60
+        System.out.println(b4); // 180, 70
+    }
+}
