ac20d924659a3c7effa0756297eb8cb2ab5caa45
MB-teacher <MB-teacher@192.168.1.157>
2020-08-14 09:39:11 +0800

    Java

--- src/main/java/com/pcschool/ocp/d04/ConstructorDemo.java
+++ src/main/java/com/pcschool/ocp/d04/ConstructorDemo.java
@@ -2,6 +2,7 @@ package com.pcschool.ocp.d04;
 
 public class ConstructorDemo {
     public static void main(String[] args) {
-        
+        Employee emp = new Employee("vincent", 40000);
+        System.out.println(emp);
     }
 }
--- src/main/java/com/pcschool/ocp/d04/Employee.java
+++ src/main/java/com/pcschool/ocp/d04/Employee.java
@@ -4,9 +4,15 @@ public class Employee {
     private String name;
     private int salary;
 
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
     @Override
     public String toString() {
         return "Employee{" + "name=" + name + ", salary=" + salary + '}';
     }
+    
     
 }
