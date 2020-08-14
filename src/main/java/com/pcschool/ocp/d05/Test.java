859717befb8aa63e69c489f688904eff79093974
MB-teacher <MB-teacher@192.168.1.157>
2020-08-14 15:26:47 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d05/Test.java
@@ -0,0 +1,19 @@
+package com.pcschool.ocp.d05;
+
+public class Test {
+    int x, y;
+
+    public Test(int x, int y) {
+        this.x = x * x;
+        this.y = y * y;
+    }
+    
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        Test t = new Test(x, y);
+        System.out.println(x + ", " + y);
+        System.out.println(t.x + ", " + t.y);
+    }
+    
+}
