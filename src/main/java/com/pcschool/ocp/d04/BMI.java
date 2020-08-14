73d21ba582e7a664c1639fee4975a5e9e81a9fd0
MB-teacher <MB-teacher@192.168.1.157>
2020-08-14 10:32:42 +0800

    Java

--- src/main/java/com/pcschool/ocp/d04/BMI.java
+++ src/main/java/com/pcschool/ocp/d04/BMI.java
@@ -1,8 +1,24 @@
 package com.pcschool.ocp.d04;
 
 public class BMI {
-    private double h; // 100 ~ 250 cm (170.0)
-    private double w; // 30 ~ 150 kg  (60.0)
+    private double h = 170; // 100 ~ 250 cm (170.0)
+    private double w = 60; // 30 ~ 150 kg  (60.0)
+
+    public BMI() {
+    }
+
+    public BMI(double h, double w) {
+        this(h);
+        if(w >= 30 && w <= 150){
+            this.w = w;
+        }
+    }
+    
+    public BMI(double h) {
+        if(h >= 100 && h <= 250){
+            this.h = h;
+        }
+    }
 
     @Override
     public String toString() {
--- src/main/java/com/pcschool/ocp/d04/ConstructorDemo4.java
+++ src/main/java/com/pcschool/ocp/d04/ConstructorDemo4.java
@@ -6,10 +6,14 @@ public class ConstructorDemo4 {
         BMI b2 = new BMI(180, 180);
         BMI b3 = new BMI(60, 170);
         BMI b4 = new BMI(180, 70);
+        BMI b5 = new BMI(190); // 身高
+        BMI b6 = new BMI(290); // 身高
         
         System.out.println(b1); // 170, 60
         System.out.println(b2); // 180, 60
         System.out.println(b3); // 170, 60
         System.out.println(b4); // 180, 70
+        System.out.println(b5); // 190, 60
+        System.out.println(b6); // 170, 60
     }
 }
