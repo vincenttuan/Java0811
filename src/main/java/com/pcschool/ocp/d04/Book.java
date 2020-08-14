d22b9f1c62f7df050f49e895712583652aa3cc88
MB-teacher <MB-teacher@192.168.1.157>
2020-08-14 10:09:00 +0800

    Java

--- src/main/java/com/pcschool/ocp/d04/Book.java
+++ src/main/java/com/pcschool/ocp/d04/Book.java
@@ -4,9 +4,13 @@ public class Book {
     private int pages = 200; // 預設最多頁數
 
     public Book() {
+        
     }
     
     public Book(int pages) {
+        if(pages > 0 && pages < this.pages){
+            this.pages = pages;
+        }
     }
 
     @Override
