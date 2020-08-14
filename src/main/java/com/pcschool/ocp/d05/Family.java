14aee0f3927648ac85025d46804c163983f94519
MB-teacher <MB-teacher@192.168.1.157>
2020-08-14 14:07:43 +0800

    Java

--- src/main/java/com/pcschool/ocp/d05/Family.java
+++ src/main/java/com/pcschool/ocp/d05/Family.java
@@ -1,9 +1,18 @@
 package com.pcschool.ocp.d05;
 
+import com.pcschool.ocp.d05.foo.Son2;
+import com.pcschool.ocp.d05.foo.Son3;
+
 public class Family {
     public static void main(String[] args) {
         Son son = new Son();
         System.out.println(son.money);
         son.play();
+        
+        Son2 son2 = new Son2();
+        son2.print();
+        
+        Son3 son3 = new Son3();
+        son3.print();
     }
 }
---
+++ src/main/java/com/pcschool/ocp/d05/foo/Son3.java
@@ -0,0 +1,10 @@
+package com.pcschool.ocp.d05.foo;
+
+import com.pcschool.ocp.d05.Father;
+
+public class Son3  {
+    public void print() {
+        Father father = new Father();
+        System.out.println(father.money);
+    }
+}
