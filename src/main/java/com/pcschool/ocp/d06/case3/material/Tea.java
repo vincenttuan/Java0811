f937084fc0c15340f80b01553f45b8fcb899aa0d
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 13:49:58 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case3/material/Tea.java
+++ src/main/java/com/pcschool/ocp/d06/case3/material/Tea.java
@@ -3,7 +3,7 @@ package com.pcschool.ocp.d06.case3.material;
 public class Tea extends Material {
 
     public Tea() {
-        this("紅茶", 15);
+        this("阿薩姆", 15);
     }
     
     public Tea(String name, int price) {
