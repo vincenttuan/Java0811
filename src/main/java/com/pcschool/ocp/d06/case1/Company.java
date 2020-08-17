46a707b9e10034e4a2b81c862303f26173a36cdc
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 09:48:35 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case1/Company.java
+++ src/main/java/com/pcschool/ocp/d06/case1/Company.java
@@ -4,9 +4,11 @@ public class Company {
     public static void main(String[] args) {
         Employee emp = new Employee();
         System.out.println(emp.getSalary());
+        
         Manager manager = new Manager();
         manager.setSalary(50000);
         System.out.println(manager.getSalary());
+        
         Manager manager2 = new Manager();
         System.out.println(manager2.getSalary());
     }
