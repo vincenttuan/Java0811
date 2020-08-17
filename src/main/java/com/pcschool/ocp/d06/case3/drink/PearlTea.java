a5ce978d7491171cf9e87f40e95796c9541b73b8
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 14:15:17 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case3/drink/PearlTea.java
+++ src/main/java/com/pcschool/ocp/d06/case3/drink/PearlTea.java
@@ -1,8 +1,6 @@
 package com.pcschool.ocp.d06.case3.drink;
 
-import com.pcschool.ocp.d06.case3.material.Milk;
 import com.pcschool.ocp.d06.case3.material.Pearl;
-import com.pcschool.ocp.d06.case3.material.Tea;
 
 public class PearlTea extends MilkTea {
 
