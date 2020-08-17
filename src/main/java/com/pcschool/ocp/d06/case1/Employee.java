dc8bd2626d589bf945e082ab1fa318a994de88c1
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 09:32:56 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d06/case1/Employee.java
@@ -0,0 +1,17 @@
+package com.pcschool.ocp.d06.case1;
+
+public class Employee {
+    private int salary;
+
+    public Employee() {
+    }
+    
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int money) {
+        this.salary = money;
+    }
+    
+}
---
+++ src/main/java/com/pcschool/ocp/d06/case1/Manager.java
@@ -0,0 +1,8 @@
+package com.pcschool.ocp.d06.case1;
+
+public class Manager {
+
+    public Manager() {
+    }
+    
+}
