f760e7103ae71b92f828635140c7be52ed65d52a
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 13:30:29 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case3/material/Material.java
+++ src/main/java/com/pcschool/ocp/d06/case3/material/Material.java
@@ -1,10 +1,23 @@
 package com.pcschool.ocp.d06.case3.material;
 
 public class Material {
- 
-	private String name;
-	 
-	private int price;
-	 
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
 }
- 
