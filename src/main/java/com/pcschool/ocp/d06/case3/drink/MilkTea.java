34b4de7f81aaff5a7a10373d3bc89dcb63cabbf2
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 14:14:59 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case3/drink/MilkTea.java
+++ src/main/java/com/pcschool/ocp/d06/case3/drink/MilkTea.java
@@ -1,14 +1,12 @@
 package com.pcschool.ocp.d06.case3.drink;
 
 import com.pcschool.ocp.d06.case3.material.Milk;
-import com.pcschool.ocp.d06.case3.material.Tea;
 
-public class MilkTea extends Drink {
+public class MilkTea extends RedTea {
 
     public MilkTea() {
         Milk milk = new Milk();
-        Tea tea = new Tea();
-        addMaterial(milk).addMaterial(tea);
+        addMaterial(milk);
     }
  
 }
--- src/main/java/com/pcschool/ocp/d06/case3/drink/PearlTea.java
+++ src/main/java/com/pcschool/ocp/d06/case3/drink/PearlTea.java
@@ -4,13 +4,11 @@ import com.pcschool.ocp.d06.case3.material.Milk;
 import com.pcschool.ocp.d06.case3.material.Pearl;
 import com.pcschool.ocp.d06.case3.material.Tea;
 
-public class PearlTea extends Drink {
+public class PearlTea extends MilkTea {
 
     public PearlTea() {
-        Milk milk = new Milk();
-        Tea tea = new Tea();
         Pearl pearl = new Pearl();
-        addMaterial(milk).addMaterial(tea).addMaterial(pearl);
+        addMaterial(pearl);
     }
  
 }
