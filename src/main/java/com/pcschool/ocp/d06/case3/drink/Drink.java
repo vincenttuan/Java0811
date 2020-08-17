209b0eda28f4b6a83eb87d553ee0c175df1283a9
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 13:53:34 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case3/drink/Drink.java
+++ src/main/java/com/pcschool/ocp/d06/case3/drink/Drink.java
@@ -1,12 +1,16 @@
 package com.pcschool.ocp.d06.case3.drink;
 
 import com.pcschool.ocp.d06.case3.material.Material;
+import java.util.ArrayList;
 
 public class Drink {
- 
-	private Material[] material;
-	 
-	private int price;
-	 
+
+    private ArrayList<Material> materials; // ArrayList 動態陣列宣告
+    private int price;
+
+    public Drink() {
+        materials = new ArrayList<>(); // ArrayList 動態陣列實體
+    }
+    
+    
 }
- 
