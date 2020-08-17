eb091fcd49c9061c2121b0e71db979d0a1ca263c
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 09:47:07 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case1/Company.java
+++ src/main/java/com/pcschool/ocp/d06/case1/Company.java
@@ -5,6 +5,9 @@ public class Company {
         Employee emp = new Employee();
         System.out.println(emp.getSalary());
         Manager manager = new Manager();
+        manager.setSalary(50000);
         System.out.println(manager.getSalary());
+        Manager manager2 = new Manager();
+        System.out.println(manager2.getSalary());
     }
 }
--- src/main/java/com/pcschool/ocp/d06/case1/Manager.java
+++ src/main/java/com/pcschool/ocp/d06/case1/Manager.java
@@ -3,6 +3,7 @@ package com.pcschool.ocp.d06.case1;
 public class Manager extends Employee {
 
     public Manager() {
+        setSalary(50000);
     }
     
 }
