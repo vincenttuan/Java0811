0fd6bd21e0c38182b120a5bac31ea75180675416
MB-teacher <MB-teacher@192.168.1.157>
2020-08-17 15:56:58 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/case3/DrinkStore2.java
+++ src/main/java/com/pcschool/ocp/d06/case3/DrinkStore2.java
@@ -6,6 +6,9 @@ import com.pcschool.ocp.d06.case3.drink.GreenPearlTea;
 import com.pcschool.ocp.d06.case3.drink.GreenTea;
 import com.pcschool.ocp.d06.case3.drink.MilkTea;
 import com.pcschool.ocp.d06.case3.drink.PearlTea;
+import com.pcschool.ocp.d06.case3.material.Milk;
+import com.pcschool.ocp.d06.case3.material.Pearl;
+import com.pcschool.ocp.d06.case3.material.Tea;
 import java.util.ArrayList;
 
 public class DrinkStore2 {
@@ -33,6 +36,15 @@ public class DrinkStore2 {
         System.out.printf("總共買了多少錢: $%,d\n", sum);
         
         // 2.成分中有奶製品(Milk)的共有多少錢 ?
+        sum = drinks.stream()
+                .filter(d -> d instanceof MilkTea || d instanceof Coffee || d instanceof PearlTea || d instanceof GreenPearlTea )
+                .mapToInt(d -> d.getPrice()).sum();
+        System.out.printf("有奶製品(Milk)總共買了多少錢: $%,d\n", sum);        
         
+        // 改良
+        sum = drinks.stream()
+                .filter(d -> d.getMaterials().stream().filter(m -> m instanceof Milk).findAny().isPresent())
+                .mapToInt(d -> d.getPrice()).sum();
+        System.out.printf("有奶製品(Milk)總共買了多少錢: $%,d\n", sum);        
     }
 }
