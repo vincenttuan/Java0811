ab37912106f137578696d33f4b20ee619a4f5ba6
MB-teacher <MB-teacher@192.168.1.157>
2020-08-18 11:43:57 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case1/food/Food.java
+++ src/main/java/com/pcschool/ocp/d07/case1/food/Food.java
@@ -22,9 +22,13 @@ public class Food {
 
     public Food addMaterial(Material material) {
         Material[] new_materials = new Material[materials.length + 1];
+        /*
         for(int i=0;i<new_materials.length-1;i++) {
             new_materials[i] = materials[i];
         }
+        */
+        // 來源陣列 src, 來源陣列起始位置 srcPos, 目的陣列 dest, 目的陣列起始位置 destPos, 資料長度 length
+        System.arraycopy(materials, 0, new_materials, 0, materials.length);
         new_materials[new_materials.length-1] = material;
         setMaterials(new_materials);
         return this;
