d70e3afcca7d3f2c87899b1e0d57bb2fc553fa07
MB-teacher <MB-teacher@192.168.1.157>
2020-08-18 14:21:32 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case3/Boss.java
+++ src/main/java/com/pcschool/ocp/d07/case3/Boss.java
@@ -1,6 +1,7 @@
 package com.pcschool.ocp.d07.case3;
 
 public class Boss extends Manager {
+    public int salary = 100000;
     public void printFire() {
         System.out.println("開除員工");
     }
--- src/main/java/com/pcschool/ocp/d07/case3/Employee.java
+++ src/main/java/com/pcschool/ocp/d07/case3/Employee.java
@@ -1,6 +1,7 @@
 package com.pcschool.ocp.d07.case3;
 
 public class Employee {
+    public int salary = 30000;
     public void printSalary() {
         System.out.println("支取薪資");
     }
