1ac77823027da0967b29b087442c9bc4f3c4849c
MB-teacher <MB-teacher@192.168.1.157>
2020-08-18 14:29:22 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case3/Company2.java
+++ src/main/java/com/pcschool/ocp/d07/case3/Company2.java
@@ -5,6 +5,9 @@ public class Company2 {
         Employee emp1 = new Employee();
         Employee emp2 = new Boss();
         System.out.println(emp1.salary);
+        emp1.job();
+        
         System.out.println(emp2.salary);
+        emp2.job();
     }
 }
