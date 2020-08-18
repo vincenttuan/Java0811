88aca2c5103e9435414984f6a417001bf3d936b7
MB-teacher <MB-teacher@192.168.1.157>
2020-08-18 10:05:15 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case1/food/Food.java
+++ src/main/java/com/pcschool/ocp/d07/case1/food/Food.java
@@ -3,24 +3,39 @@ package com.pcschool.ocp.d07.case1.food;
 import com.pcschool.ocp.d07.case1.material.Material;
 
 public class Food {
- 
-	private String name;
-	 
-	private Material[] materials;
-	 
-	public Food() {
-	}
-	 
-	public Food(String name) {
-	}
-	 
-	public int getPrice() {
-		return 0;
-	}
-	 
-	public Food addMaterial(Material material) {
-		return null;
-	}
-	 
+
+    private String name;
+
+    private Material[] materials;
+
+    public Food() {
+    }
+
+    public Food(String name) {
+    }
+
+    public int getPrice() {
+        return 0;
+    }
+
+    public Food addMaterial(Material material) {
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Material[] getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(Material[] materials) {
+        this.materials = materials;
+    }
+
 }
- 
--- src/main/java/com/pcschool/ocp/d07/case1/material/Material.java
+++ src/main/java/com/pcschool/ocp/d07/case1/material/Material.java
@@ -1,16 +1,31 @@
 package com.pcschool.ocp.d07.case1.material;
 
 public class Material {
- 
-	private String name;
-	 
-	private int price;
-	 
-	public Material() {
-	}
-	 
-	public Material(String name, int price) {
-	}
-	 
+    private String name;
+    private int price;
+
+    public Material() {
+    }
+
+    public Material(String name, int price) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+
 }
- 
