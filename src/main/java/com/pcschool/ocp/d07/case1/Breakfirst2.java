24a83bcd179fe852b21454b81452467c205af75f
MB-teacher <MB-teacher@192.168.1.157>
2020-08-18 11:56:53 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d07/case1/Breakfirst2.java
@@ -0,0 +1,13 @@
+package com.pcschool.ocp.d07.case1;
+
+import com.pcschool.ocp.d07.case1.food.Food;
+import com.pcschool.ocp.d07.case1.food.Hamburger;
+import com.pcschool.ocp.d07.case1.food.Sandwich;
+
+public class Breakfirst2 {
+    public static void main(String[] args) {
+        Food[] foods = {new Sandwich("火腿蛋"), new Sandwich("火腿蛋"), new Hamburger("牛肉蛋堡")};
+        // 請問我要付多少錢 ?
+        
+    }
+}
