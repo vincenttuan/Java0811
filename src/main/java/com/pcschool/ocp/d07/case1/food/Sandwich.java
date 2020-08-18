c458efd4e1eb3808790c46376cc2d8aafe442e51
MB-teacher <MB-teacher@192.168.1.157>
2020-08-18 10:48:39 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case1/food/Sandwich.java
+++ src/main/java/com/pcschool/ocp/d07/case1/food/Sandwich.java
@@ -1,9 +1,18 @@
 package com.pcschool.ocp.d07.case1.food;
 
+import com.pcschool.ocp.d07.case1.material.Egg;
+import com.pcschool.ocp.d07.case1.material.Ham;
+import com.pcschool.ocp.d07.case1.material.Material;
+
 public class Sandwich extends Food {
 
     public Sandwich(String name) {
         super(name);
+        // 定義食材
+        Material[] materials = new Material[2];
+        materials[0] = new Ham();
+        materials[1] = new Egg();
+        setMaterials(materials); // 將食材陣列透過setMaterials設定到Food的materials物件變數中
     }
  
 }
