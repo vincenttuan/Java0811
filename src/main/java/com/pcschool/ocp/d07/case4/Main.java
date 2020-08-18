159ac516535e8e9d25711f677b255b0fa899efcc
MB-teacher <MB-teacher@192.168.1.157>
2020-08-18 15:28:47 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case4/Main.java
+++ src/main/java/com/pcschool/ocp/d07/case4/Main.java
@@ -1,5 +1,6 @@
 package com.pcschool.ocp.d07.case4;
 
+import com.google.gson.Gson;
 import java.io.File;
 import java.util.Scanner;
 
@@ -8,5 +9,10 @@ public class Main {
         // 取得 json 字串
         String path = "src\\main\\java\\com\\pcschool\\ocp\\d07\\case4\\programmer.json";
         String json = new Scanner(new File(path)).useDelimiter("\\A").next();
+        //System.out.println(json);
+        // 透過 Gson 將 json 資料匯入到 Programmer[] 陣列中
+        Programmer[] programmers = new Gson().fromJson(json, Programmer[].class);
+        // 有幾筆資料 ?
+        System.out.printf("資料筆數: %d\n", programmers.length);
     }
 }
