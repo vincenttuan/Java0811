620e994c8897db4a683221edc32dcc3484672ca5
MB-teacher <MB-teacher@192.168.1.157>
2020-08-18 10:49:59 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case1/food/Hamburger.java
+++ src/main/java/com/pcschool/ocp/d07/case1/food/Hamburger.java
@@ -1,9 +1,18 @@
 package com.pcschool.ocp.d07.case1.food;
 
+import com.pcschool.ocp.d07.case1.material.Beef;
+import com.pcschool.ocp.d07.case1.material.Egg;
+import com.pcschool.ocp.d07.case1.material.Material;
+
 public class Hamburger extends Food {
 
     public Hamburger(String name) {
         super(name);
+        // 定義食材
+        Material[] materials = new Material[2];
+        materials[0] = new Beef();
+        materials[1] = new Egg();
+        setMaterials(materials);
     }
  
 }
