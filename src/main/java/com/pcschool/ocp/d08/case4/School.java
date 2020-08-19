69e5722f3ad07d085816dc342fb47ad326655a40
MB-teacher <MB-teacher@192.168.1.157>
2020-08-19 13:31:55 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/case4/School.java
@@ -0,0 +1,19 @@
+package com.pcschool.ocp.d08.case4;
+
+public class School {
+    public static void main(String[] args) {
+        Person[] persons = {
+            new Teacher("T1", 30, 23.3, 70000),
+            new Teacher("T2", 35, 25.4, 90000),
+            new Student("S1", 16, 17.5),
+            new Student("S2", 17, 22.5),
+            new Student("S3", 18, 24.5),
+        };
+        // 請問平均年齡 ?
+        
+        // 請問 Teacher 平均薪資 ?
+        
+        // 請問 學生 平均 BMI ?
+        
+    }
+}
