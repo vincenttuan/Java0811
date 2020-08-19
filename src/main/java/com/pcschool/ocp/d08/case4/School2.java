a98d769e2cfab53a3741d20e7c8daf1b851626a8
MB-teacher <MB-teacher@192.168.1.157>
2020-08-19 15:16:32 +0800

    Java

--- src/main/java/com/pcschool/ocp/d08/case4/School2.java
+++ src/main/java/com/pcschool/ocp/d08/case4/School2.java
@@ -1,5 +1,8 @@
 package com.pcschool.ocp.d08.case4;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class School2 {
     public static void main(String[] args) {
         Person[] p1 = {
@@ -18,5 +21,14 @@ public class School2 {
         };
         Person[][] persons = {p1, p2};
         // 求 Teacher 的平均薪資 ?
+        double salaryAvg = Arrays.stream(persons)
+                .flatMap(Stream::of)
+                .filter(p -> p instanceof Teacher)
+                .map(p -> (Teacher)p)
+                .mapToInt(t -> t.getSalary().getMoney())
+                //.peek(System.out::println)
+                .average()
+                .getAsDouble();
+        System.out.println(salaryAvg);
     }
 }
