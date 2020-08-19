7b8b069b7b3c49eea162d22c34b9b00ebc1cc9b8
MB-teacher <MB-teacher@192.168.1.157>
2020-08-19 15:31:35 +0800

    Java

--- src/main/java/com/pcschool/ocp/d08/BallStore.java
+++ src/main/java/com/pcschool/ocp/d08/BallStore.java
@@ -2,7 +2,7 @@ package com.pcschool.ocp.d08;
 
 public class BallStore {
     public static void main(String[] args) {
-        Ball b1 = new Ball("Red", 100);
+        Ball b1 = new Ball("Red", 200);
         Ball b2 = new Ball("Red", 100);
         System.out.println(b1);
         System.out.println(b2);
