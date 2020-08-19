f91e1d22143e197c91ecda91c77a440d14fcb206
MB-teacher <MB-teacher@192.168.1.157>
2020-08-19 15:49:07 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/BallStore2.java
@@ -0,0 +1,17 @@
+package com.pcschool.ocp.d08;
+
+public class BallStore2 {
+    public static void main(String[] args) {
+        final Ball target = new Ball("Red", 100);
+        Ball[] balls = {
+            new Ball("Red", 100), 
+            new Ball("Blue", 100), 
+            new Ball("Red", 100), 
+            new Ball("Yellow", 300), 
+            new Ball("Red", 200)
+        };
+        // 請將符合 target 的 Ball 列出 ?
+        
+        
+    }
+}
