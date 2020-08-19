0efe949479e6cd76b1095b93b95c6bcb9638e9c8
MB-teacher <MB-teacher@192.168.1.157>
2020-08-19 11:35:05 +0800

    Java

--- src/main/java/com/pcschool/ocp/d08/army/ArmyMain.java
+++ src/main/java/com/pcschool/ocp/d08/army/ArmyMain.java
@@ -1,5 +1,19 @@
 package com.pcschool.ocp.d08.army;
 
 public class ArmyMain {
-    
+    public static void main(String[] args) {
+        Airplane[] airplanes = {new Fighter(), new Fighter(), new Fighter()};
+        Car[] cars = {new Tank(), new Truck(), new Truck(), new Tank()};
+        Weapon[] weapons = {new Fighter(), new Tank()};
+        // cars 要統一發射武器
+        for(Car car : cars) {
+            if(car instanceof Tank) {
+                ((Tank)car).shoot();
+            }
+        }
+        // weapons 要統一發射武器
+        for(Weapon w : weapons) {
+            w.shoot();
+        }
+    }
 }
