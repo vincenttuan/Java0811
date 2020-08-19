850e516a757a40768b8f42a3a8d594c76b0871b9
MB-teacher <MB-teacher@192.168.1.157>
2020-08-19 15:27:20 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/Ball.java
@@ -0,0 +1,41 @@
+package com.pcschool.ocp.d08;
+
+public class Ball {
+    private String color;
+    private int price;
+
+    public Ball(String color, int price) {
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+    
+    
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" + "color=" + color + ", price=" + price + '}';
+    }
+
+    
+}
