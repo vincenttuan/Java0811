039decee17420894706dd57dee2b51e644664640
MB-teacher <MB-teacher@192.168.1.157>
2020-08-20 11:17:10 +0800

    Java

--- src/main/java/com/pcschool/ocp/d09/case4/ConfirmMain.java
+++ src/main/java/com/pcschool/ocp/d09/case4/ConfirmMain.java
@@ -2,8 +2,23 @@ package com.pcschool.ocp.d09.case4;
 
 public class ConfirmMain {
     public static void main(String[] args) {
-        Confirm confirm1 = (n) -> n >= 60;
-        Confirm confirm2 = (n) -> n >= 18 && n < 23;
+        Confirm confirm1 = new Confirm() {
+            @Override
+            public boolean isPass(int n) {
+                return n >= 60;
+            }
+        };
         
+        Confirm confirm2 = new Confirm() {
+            @Override
+            public boolean isPass(int n) {
+                return n >= 18 && n < 23;
+            }
+        };
+        
+        Confirm confirm3 = (n) -> n >= 60;
+        Confirm confirm4 = (n) -> n >= 18 && n < 23;
+        
+        System.out.println(confirm3.isPass(80));
     }
 }
