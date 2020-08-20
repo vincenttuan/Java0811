e3096a31de821ad9c4748b8cab5ee650f908a671
MB-teacher <MB-teacher@192.168.1.157>
2020-08-20 10:15:39 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d09/case2/LottoStore.java
@@ -0,0 +1,13 @@
+package com.pcschool.ocp.d09.case2;
+
+public class LottoStore {
+    public static void main(String[] args) {
+        // 實作 lotto 介面以開出 0~9 一個數字
+        
+        // xxx.printNumber(); // 7
+        
+        // 實作 lotto 介面以開出 0~9 四個數字
+        
+        // xxx.printNumber(); // 3754
+    }
+}
