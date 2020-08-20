a6171c9ead6d838ec79d7593418b1a92724a8f47
MB-teacher <MB-teacher@192.168.1.157>
2020-08-20 11:30:39 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d09/case5/Car.java
@@ -0,0 +1,10 @@
+package com.pcschool.ocp.d09.case5;
+
+public class Car implements Motor{
+
+    @Override
+    public int speed() {
+        return 180;
+    }
+    
+}
---
+++ src/main/java/com/pcschool/ocp/d09/case5/Motor.java
@@ -0,0 +1,5 @@
+package com.pcschool.ocp.d09.case5;
+
+public interface Motor {
+    int speed();
+}
