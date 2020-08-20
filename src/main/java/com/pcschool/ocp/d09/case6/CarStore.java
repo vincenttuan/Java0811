7d64493224ae0242b9af4c9ecea7c6eeb5fcc1f3
MB-teacher <MB-teacher@192.168.1.157>
2020-08-20 11:58:32 +0800

    Java

--- src/main/java/com/pcschool/ocp/d09/case6/CarStore.java
+++ src/main/java/com/pcschool/ocp/d09/case6/CarStore.java
@@ -9,16 +9,19 @@ public class CarStore {
                 return new Car(name);
             }
         };
+        System.out.println("ride1: " + ride1);
         Car car1 = ride1.getCar("BMW");
         System.out.println(car1);
         
         // lambda
         Rideable ride2 = (name) -> new Car(name);
+        System.out.println("ride2: " + ride2);
         Car car2 = ride2.getCar("BMW");
         System.out.println(car2);
         
         // lambda ::
         Rideable ride3 = Car::new;
+        System.out.println("ride3: " + ride3);
         Car car3 = ride3.getCar("BMW");
         System.out.println(car3);
     }
