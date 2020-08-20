315481d927695e279227238644f05551bc8abaf3
MB-teacher <MB-teacher@192.168.1.157>
2020-08-20 11:12:47 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d09/case4/Confirm.java
@@ -0,0 +1,6 @@
+package com.pcschool.ocp.d09.case4;
+
+@FunctionalInterface
+public interface Confirm {
+    boolean isPass(int n);
+}
---
+++ src/main/java/com/pcschool/ocp/d09/case4/ConfirmMain.java
@@ -0,0 +1,9 @@
+package com.pcschool.ocp.d09.case4;
+
+public class ConfirmMain {
+    public static void main(String[] args) {
+        Confirm confirm1 = (n) -> n >= 60;
+        Confirm confirm2 = (n) -> n >= 18 && n < 23;
+        
+    }
+}
