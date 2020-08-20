a4bac331bbd1f1a5adc49a5d2cefaf5271f4c004
MB-teacher <MB-teacher@192.168.1.157>
2020-08-20 12:04:11 +0800

    Java

--- src/main/java/com/pcschool/ocp/d09/case5/CarStore.java
+++ src/main/java/com/pcschool/ocp/d09/case5/CarStore.java
@@ -3,8 +3,9 @@ package com.pcschool.ocp.d09.case5;
 public class CarStore {
     public static void main(String[] args) {
         Motor m1 = new Car();
-        Motor m2 = Car::new ;
+        Motor m2 = () -> System.out.println(180);
         m1.speed();
         m2.speed();
+        
     }
 }
