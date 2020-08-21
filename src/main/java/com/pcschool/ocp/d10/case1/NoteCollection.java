c179776b29dfda30214813406f5b7ce205eab8fc
MB-teacher <MB-teacher@192.168.1.157>
2020-08-21 09:41:13 +0800

    Java

--- src/main/java/com/pcschool/ocp/d10/case1/NoteCollection.java
+++ src/main/java/com/pcschool/ocp/d10/case1/NoteCollection.java
@@ -2,8 +2,12 @@ package com.pcschool.ocp.d10.case1;
 
 public class NoteCollection {
     public static void main(String[] args) {
-        Note n1 = new Note(100);
-        Note n2 = new Note(90);
-        
+        Note n3 = new Note(80);
+        Note n2 = new Note(90, n3);
+        Note n1 = new Note(100, n2);
+        System.out.println(n1);
+        System.out.println(n1.getData());
+        System.out.println(n1.getNextNode().getData());
+        System.out.println(n1.getNextNode().getNextNode().getData());
     }
 }
