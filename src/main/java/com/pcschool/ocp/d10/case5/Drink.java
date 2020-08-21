62b158c63579f16842c516c80005678a893445f0
MB-teacher <MB-teacher@192.168.1.157>
2020-08-21 13:26:52 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d10/case5/Drink.java
@@ -0,0 +1,64 @@
+package com.pcschool.ocp.d10.case5;
+
+import java.util.Objects;
+
+public class Drink {
+    private String name;
+    private int price;
+
+    public Drink(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + Objects.hashCode(this.name);
+        hash = 43 * hash + this.price;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Drink other = (Drink) obj;
+        if (this.price != other.price) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" + "name=" + name + ", price=" + price + '}';
+    }
+    
+}
