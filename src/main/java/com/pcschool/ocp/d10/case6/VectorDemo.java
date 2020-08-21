44d954fd2dfcfe75ed0f94000066b46fb4a31004
MB-teacher <MB-teacher@192.168.1.157>
2020-08-21 15:18:54 +0800

    Java

--- src/main/java/com/pcschool/ocp/d10/case6/VectorDemo.java
+++ src/main/java/com/pcschool/ocp/d10/case6/VectorDemo.java
@@ -5,11 +5,11 @@ import java.util.Vector;
 
 public class VectorDemo {
     public static void main(String[] args) throws Exception {
-        Vector<Integer> v = new Vector<>();
-        for(int i=1;i<=25;i++) {
+        Vector<Integer> v = new Vector<>(25, 5);
+        for(int i=1;i<=26;i++) {
             v.add(new Random().nextInt(100));
             System.out.printf("capacity: %d  size: %02d  %s\n", v.capacity(), v.size(), v);
-            Thread.sleep(1000);
+            Thread.sleep(500);
         }
         
     }
