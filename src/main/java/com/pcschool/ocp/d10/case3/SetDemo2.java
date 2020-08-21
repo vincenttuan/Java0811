76469af14aa7eb06a51ae6e62e71cdff00f97bc5
MB-teacher <MB-teacher@192.168.1.157>
2020-08-21 10:33:20 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d10/case3/SetDemo2.java
@@ -0,0 +1,26 @@
+package com.pcschool.ocp.d10.case3;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo2 {
+    public static void main(String[] args) {
+        // 集合預設可以放入任意 Object 資料
+        Set set = new LinkedHashSet();
+        set.add("國文");
+        set.add(new Integer(100));
+        set.add("英文");
+        set.add(80); // new Integer(80) 或 Integer.valueOf(80)
+        set.add("數學");
+        set.add(90);
+        System.out.println(set);
+        // 尋訪器(遍歷每一個元素)
+        Iterator iter = set.iterator();
+        while (iter.hasNext()) { // 確認是否有元素 ?
+            Object next = iter.next(); // 取得元素內容
+            System.out.println(next);  // 印出元素內容
+        }
+        
+    }
+}
