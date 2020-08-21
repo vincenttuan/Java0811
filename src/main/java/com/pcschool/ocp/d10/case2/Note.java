1a2b3201cc4475d7aa98f2e70c4f9633e3be1a6e
MB-teacher <MB-teacher@192.168.1.157>
2020-08-21 09:48:33 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d10/case2/Note.java
@@ -0,0 +1,51 @@
+package com.pcschool.ocp.d10.case2;
+
+public class Note {
+    private Note previousNote;
+    private int data;
+    private Note nextNote;
+
+    public Note(int data) {
+        this(null, data, null);
+    }
+
+    public Note(Note previousNote, int data, Note nextNote) {
+        this.previousNote = previousNote;
+        this.data = data;
+        this.nextNote = nextNote;
+    }
+
+    public Note getPreviousNote() {
+        return previousNote;
+    }
+
+    public void setPreviousNote(Note previousNote) {
+        this.previousNote = previousNote;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Note getNextNote() {
+        return nextNote;
+    }
+
+    public void setNextNote(Note nextNote) {
+        this.nextNote = nextNote;
+    }
+
+    public String printNextNotes() {
+        return "Note{" + "data=" + data + ", nextNote=" + nextNote + '}';
+    }
+    
+    public String printPreviousNotes() {
+        return "Note{" + "data=" + data + ", previousNote=" + previousNote + '}';
+    }
+
+    
+}
