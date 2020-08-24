35928b1202fbbb0de484d98bd44f057967094b46
MB-teacher <MB-teacher@192.168.1.157>
2020-08-24 11:39:15 +0800

    Java

--- src/main/java/com/pcschool/ocp/d11/case2/ItemGroupDemo.java
+++ src/main/java/com/pcschool/ocp/d11/case2/ItemGroupDemo.java
@@ -1,5 +1,21 @@
 package com.pcschool.ocp.d11.case2;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 public class ItemGroupDemo {
-    
+
+    public static void main(String[] args) {
+        List<Item> items = Arrays.asList(
+                new Item("apple", 10, new BigDecimal("9.99")),
+                new Item("banana", 20, new BigDecimal("19.99")),
+                new Item("orang", 10, new BigDecimal("29.99")),
+                new Item("watermelon", 10, new BigDecimal("29.99")),
+                new Item("papaya", 20, new BigDecimal("9.99")),
+                new Item("apple", 10, new BigDecimal("9.99")),
+                new Item("banana", 10, new BigDecimal("19.99")),
+                new Item("apple", 20, new BigDecimal("9.99"))
+        );
+    }
 }
