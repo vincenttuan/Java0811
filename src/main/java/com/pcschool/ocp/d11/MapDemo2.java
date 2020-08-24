316001869036078a2d724d347ca76197faa620e3
MB-teacher <MB-teacher@192.168.1.157>
2020-08-24 09:57:00 +0800

    Java

--- src/main/java/com/pcschool/ocp/d11/MapDemo2.java
+++ src/main/java/com/pcschool/ocp/d11/MapDemo2.java
@@ -1,5 +1,6 @@
 package com.pcschool.ocp.d11;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -25,6 +26,13 @@ public class MapDemo2 {
         // 走訪元素 3
         Set<Entry<String, Integer>> entrySet =  exams.entrySet();
         for(Entry<String, Integer> entry : entrySet) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+        System.out.println();
+        // 走訪元素 4 (可以利用 iterator.remobe() 來移除元素)
+        Iterator<Map.Entry<String, Integer>> iterator = exams.entrySet().iterator();
+        while(iterator.hasNext()) {
+            Entry<String, Integer> entry = iterator.next();
             System.out.println(entry.getKey() + ":" + entry.getValue());
         }
         System.out.println();
