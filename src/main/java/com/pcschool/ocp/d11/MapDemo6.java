4509cea8aa54eabaea29bee0e9e5a9dc8544c09a
MB-teacher <MB-teacher@192.168.1.157>
2020-08-24 10:55:11 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d11/MapDemo6.java
@@ -0,0 +1,14 @@
+package com.pcschool.ocp.d11;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapDemo6 {
+    public static void main(String[] args) {
+        List<String> items = Arrays.asList("蘋果", "蘋果", "香蕉", "蘋果", "柳丁", "香蕉", "椰子");
+        /*
+            如何撰寫一個根據數量作為key值的 groupingBy 群組程式，讓其分類結果變成一個 Map，其結果如下：
+            {1=[柳丁, 椰子], 2=[香蕉], 3=[蘋果]}
+        */
+    }
+}
