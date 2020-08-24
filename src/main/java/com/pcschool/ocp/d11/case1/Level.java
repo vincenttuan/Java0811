6a7265225a3450073d290ddb4d4503b372c55ec4
MB-teacher <MB-teacher@192.168.1.157>
2020-08-24 11:27:13 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d11/case1/Level.java
@@ -0,0 +1,36 @@
+package com.pcschool.ocp.d11.case1;
+
+public class Level {    
+    private int score;
+    private String name;
+
+    public Level() {
+    }
+
+    public Level(int score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Level{" + "score=" + score + ", name=" + name + '}';
+    }
+    
+}
