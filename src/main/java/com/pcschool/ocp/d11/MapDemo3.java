3486db7ebf3f89c880833c176581a6bbbeded934
MB-teacher <MB-teacher@192.168.1.157>
2020-08-24 10:13:53 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d11/MapDemo3.java
@@ -0,0 +1,22 @@
+package com.pcschool.ocp.d11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MapDemo3 {
+    public static void main(String[] args) {
+        List<String> items = Arrays.asList("apple", "apple", "banana", "apple", "orange", "apple", "papaya", "banana");
+        System.out.println(items);
+        /* 
+            分組:
+            apple:4, banana:2, orange=1, papaya=1
+        */
+        System.out.println(
+                items.stream()
+                     .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+        );
+        
+    }
+}
