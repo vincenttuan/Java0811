05c976f76bdfb1ded3de69113d921381b53b08eb
MB-teacher <MB-teacher@192.168.1.157>
2020-08-25 09:54:38 +0800

    Java

---
+++ src/test/java/d13/TestSquare.java
@@ -0,0 +1,24 @@
+package d13;
+
+import com.pcschool.ocp.d13_assert.Square;
+import org.junit.Assert;
+import org.junit.Test;
+public class TestSquare {
+    
+    @Test
+    public void test1() { // 測試梯形面積
+        // 輸入參數
+        int x = 10;
+        int y = 20;
+        int h = 4;
+        // 期望結果
+        int exp = 60;
+        // 實際結果
+        Square sq = new Square();
+        int act = sq.getArea(x, y, h);
+        
+        // 驗證
+        Assert.assertEquals(exp, act);
+    }
+    
+}
