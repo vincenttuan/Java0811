03772db17c61d0e82517a23779cadd074bcdeb7c
MB-teacher <MB-teacher@192.168.1.157>
2020-08-25 09:42:17 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d13_assert/Square.java
@@ -0,0 +1,9 @@
+package com.pcschool.ocp.d13_assert;
+
+public class Square {
+    // 梯形面積
+    public int getArea(int x, int y, int h) {
+        int area = (x + y) * h / 2;
+        return area;
+    }
+}
