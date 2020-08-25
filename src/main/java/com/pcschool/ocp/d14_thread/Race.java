253ef49d1e57fe5e75e3b0e298354ad3dcb3e5fa
MB-teacher <MB-teacher@192.168.1.157>
2020-08-25 11:56:15 +0800

    Java

--- src/main/java/com/pcschool/ocp/d14_thread/Race.java
+++ src/main/java/com/pcschool/ocp/d14_thread/Race.java
@@ -1,7 +1,7 @@
 package com.pcschool.ocp.d14_thread;
 
 public class Race extends Thread {
-
+    private static boolean gameover;
     public Race(String string) {
         super(string);
     }
@@ -13,9 +13,10 @@ public class Race extends Thread {
     
     private void job() {
         String tName = Thread.currentThread().getName();
-        for(int i=1;i<=1000;i++) {
+        for(int i=1;i<=1000 && !gameover;i++) {
             System.out.printf("%s 跑了 %d 步\n", tName, i);
         }
+        gameover = true;
     }
     
 }
