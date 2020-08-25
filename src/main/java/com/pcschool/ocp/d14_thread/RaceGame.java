911e480782a7df8b2a01791e99d0a8ab7372e8df
MB-teacher <MB-teacher@192.168.1.157>
2020-08-25 11:46:07 +0800

    Java

--- src/main/java/com/pcschool/ocp/d14_thread/RaceGame.java
+++ src/main/java/com/pcschool/ocp/d14_thread/RaceGame.java
@@ -6,6 +6,9 @@ public class RaceGame {
         System.out.printf("%s 舉辦了龜兔賽跑的比賽\n", tName);
         Race r1 = new Race("烏龜");
         Race r2 = new Race("兔子");
+        // 參與人員(有幾條執行緒在運行)
+        int count = Thread.activeCount();
+        System.out.printf("參與人員: %d\n", count);
         r1.start();
         r2.start();
     }
