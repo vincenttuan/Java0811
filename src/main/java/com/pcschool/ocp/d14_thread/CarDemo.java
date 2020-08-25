6a49c2d04f625bd559519cc1f052153e0c791fac
MB-teacher <MB-teacher@192.168.1.157>
2020-08-25 15:35:56 +0800

    Java

--- src/main/java/com/pcschool/ocp/d14_thread/CarDemo.java
+++ src/main/java/com/pcschool/ocp/d14_thread/CarDemo.java
@@ -6,7 +6,7 @@ import java.util.stream.IntStream;
 public class CarDemo {
     public static void main(String[] args) {
         Runnable r = () -> System.out.println("吃午飯");
-        CyclicBarrier cb = new CyclicBarrier(4, r);
+        CyclicBarrier cb = new CyclicBarrier(1, r);
         IntStream.rangeClosed(0, 3).forEach(i -> {
             System.out.printf("第 %d 台車預備\n", i);
             new Car(cb).start();
