9bc0d2730c3bef3ffdde445905d443bc491e7eb2
MB-teacher <MB-teacher@192.168.1.157>
2020-08-26 15:10:30 +0800

    Java

--- src/main/java/com/pcschool/ocp/d16_derby/case4_book/BookClient.java
+++ src/main/java/com/pcschool/ocp/d16_derby/case4_book/BookClient.java
@@ -1,10 +1,74 @@
 package com.pcschool.ocp.d16_derby.case4_book;
 
+import java.util.Scanner;
+
 public class BookClient {
+    private static BookCRUD crud = new BookCRUD();
     public static void main(String[] args) {
-        System.gc();
-        BookCRUD crud = new BookCRUD();
-        crud.queryAll().forEach(System.out::println);
-        System.out.println(crud.getBook(1));
+        menu();
     }
+    
+    public static void menu() {
+        System.out.println("1.新增紀錄");
+        System.out.println("2.修改紀錄");
+        System.out.println("3.刪除紀錄");
+        System.out.println("4.查詢單筆");
+        System.out.println("5.查詢全部");
+        System.out.println("9.離開系統");
+        System.out.println("-------------");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("請選擇:");
+        int choice = sc.nextInt();
+        if (choice == 9) {
+            return;
+        }
+        service(choice);
+        menu();
+    }
+    
+    public static void service(int choice) {
+        switch(choice) {
+            case 1:
+                add();
+                break;
+            case 2:
+                update();
+                break;
+            case 3:
+                delete();
+                break;
+            case 4:
+                get();
+                break;
+            case 5:
+                query();
+                break;    
+        }
+    }
+    
+    // 新增紀錄
+    public static void add() {
+        
+    }
+    
+    // 修改紀錄
+    public static void update() {
+        
+    }
+    
+    // 刪除錄
+    public static void delete() {
+        
+    }
+    
+    // 查詢單筆
+    public static void get() {
+        
+    }
+    
+    // 查詢多筆
+    public static void query() {
+        
+    }
+    
 }
