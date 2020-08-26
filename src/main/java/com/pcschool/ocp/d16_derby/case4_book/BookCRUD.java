b18ab114254f9586951e53e9d15b55348eb73ffe
MB-teacher <MB-teacher@192.168.1.157>
2020-08-26 14:23:45 +0800

    Java

--- src/main/java/com/pcschool/ocp/d16_derby/case4_book/BookCRUD.java
+++ src/main/java/com/pcschool/ocp/d16_derby/case4_book/BookCRUD.java
@@ -1,6 +1,7 @@
 package com.pcschool.ocp.d16_derby.case4_book;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import java.util.List;
 public class BookCRUD {
     private BookDao dao = BookDao.getInstance();
     private Connection conn = dao.getConn();
+    private Object pstmt;
     
     // 查詢所有 book 資料
     public List<Book> queryAll() {
@@ -31,4 +33,21 @@ public class BookCRUD {
         return books;
     }
     
+    public Book getBook(int id) {
+        Book book = null;
+        String sql = "SELECT id, bname, price, ts FROM book WHERE id = ?";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()) {
+                book = new Book(rs.getInt("id"), 
+                                     rs.getString("bname"), 
+                                     rs.getInt("price"), 
+                                     rs.getTimestamp("ts"));
+            }
+        } catch (Exception e) {
+        }
+        return book;
+        
+    }
 }
