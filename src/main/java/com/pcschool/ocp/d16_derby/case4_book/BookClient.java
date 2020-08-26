1cdfb4eaf15a8d3f7b6725d456ef5214bde9623f
MB-teacher <MB-teacher@192.168.1.157>
2020-08-26 14:15:10 +0800

    Java

--- src/main/java/com/pcschool/ocp/d16_derby/case4_book/BookClient.java
+++ src/main/java/com/pcschool/ocp/d16_derby/case4_book/BookClient.java
@@ -2,6 +2,8 @@ package com.pcschool.ocp.d16_derby.case4_book;
 
 public class BookClient {
     public static void main(String[] args) {
-        
+        System.gc();
+        BookCRUD crud = new BookCRUD();
+        crud.queryAll().forEach(System.out::println);
     }
 }
