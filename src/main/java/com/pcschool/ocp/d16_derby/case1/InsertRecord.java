1b26b20417dba443d7b5d25f854fd94bcee6eaec
MB-teacher <MB-teacher@192.168.1.157>
2020-08-26 11:53:40 +0800

    Java

--- src/main/java/com/pcschool/ocp/d16_derby/case1/InsertRecord.java
+++ src/main/java/com/pcschool/ocp/d16_derby/case1/InsertRecord.java
@@ -2,6 +2,7 @@ package com.pcschool.ocp.d16_derby.case1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class InsertRecord {
     public static void main(String[] args) throws Exception {
@@ -10,6 +11,19 @@ public class InsertRecord {
         String password = "app";
         // 建立資料庫連線
         Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+        // 建立預編敘述物件
+        String sql = "INSERT INTO book(bname, price) VALUES(?, ?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        // 設定資料表欄位內容(1代表第1個問號的內容, 2代表第2個問號的內容, ...)
+        pstmt.setString(1, "VB"); 
+        pstmt.setInt(2, 300);
+        // 進行更新 (rowcount 會得到異動筆數)
+        int rowcount = pstmt.executeUpdate();
+        if(rowcount > 0) {
+            System.out.printf("新增成功: %d\n", rowcount);
+        } else {
+            System.out.printf("新增失敗: %d\n", rowcount);
+        }
         
     }
 }
