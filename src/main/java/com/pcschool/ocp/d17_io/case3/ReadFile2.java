538a1dd6cf661c8f827762bd6cbef7ea9ae10cbe
MB-teacher <MB-teacher@192.168.1.157>
2020-08-27 10:36:18 +0800

    Java

--- src/main/java/com/pcschool/ocp/d17_io/case3/ReadFile2.java
+++ src/main/java/com/pcschool/ocp/d17_io/case3/ReadFile2.java
@@ -1,12 +1,13 @@
 package com.pcschool.ocp.d17_io.case3;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
-public class ReadFile {
+public class ReadFile2 {
     public static void main(String[] args) {
         File file = new File("src\\main\\java\\com\\pcschool\\ocp\\d17_io\\report.txt");
-        try(FileReader fis = new FileReader(file);) { // 預設抓取 ANSI 編碼的內容
+        try(InputStreamReader fis = new InputStreamReader(new FileInputStream(file), "UTF-8");) { // 將 ANSI 轉 UTF-8
             StringBuilder sb = new StringBuilder();
             char[] cbuf = new char[1];
             while (fis.read(cbuf) != -1) {                
