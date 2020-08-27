0d35016d91fac3f313fffe509c449e787cb83963
MB-teacher <MB-teacher@192.168.1.157>
2020-08-27 11:36:48 +0800

    Java

--- src/main/java/com/pcschool/ocp/d17_io/case4/ReadWebImage2.java
+++ src/main/java/com/pcschool/ocp/d17_io/case4/ReadWebImage2.java
@@ -4,13 +4,16 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
-public class ReadWebImage {
+public class ReadWebImage2 {
     public static void main(String[] args) throws Exception {
-        File file = new File("src\\main\\java\\com\\pcschool\\ocp\\d17_io\\car.jpg");
-        String path = "https://www.playcar.org/uploads/others/63928_c229c860-e99f-42ab-9537-df1bd35e3255.jpg";
+        File file = new File("src\\main\\java\\com\\pcschool\\ocp\\d17_io\\car2.jpg");
+        String path = "https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_960_720.jpg";
         URL url = new URL(path);
-        try(InputStream is = url.openStream(); // 取得網路串流
+        URLConnection conn = url.openConnection();
+        conn.setRequestProperty("USER-AGENT", "隨便寫");
+        try(InputStream is = conn.getInputStream(); // 取得網路串流
             FileOutputStream fos = new FileOutputStream(file);) { 
             byte[] b = new byte[1];
             while (is.read(b) != -1) { // 將資料一個 byte 一個 byte 的讀進 byte[] b               
