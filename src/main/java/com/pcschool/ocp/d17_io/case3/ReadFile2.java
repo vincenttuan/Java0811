4f030ef7ccec790090d18c7716fe059a1ae56952
MB-teacher <MB-teacher@192.168.1.157>
2020-08-27 10:38:49 +0800

    Java

--- src/main/java/com/pcschool/ocp/d17_io/case3/ReadFile2.java
+++ src/main/java/com/pcschool/ocp/d17_io/case3/ReadFile2.java
@@ -3,11 +3,12 @@ package com.pcschool.ocp.d17_io.case3;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class ReadFile2 {
     public static void main(String[] args) {
         File file = new File("src\\main\\java\\com\\pcschool\\ocp\\d17_io\\report.txt");
-        try(InputStreamReader fis = new InputStreamReader(new FileInputStream(file), "UTF-8");) { // 將 ANSI 轉 UTF-8
+        try(InputStreamReader fis = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);) { // 將 ANSI 轉 UTF-8
             StringBuilder sb = new StringBuilder();
             char[] cbuf = new char[1];
             while (fis.read(cbuf) != -1) {                
